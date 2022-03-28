6e57cd886692dcbcb5b1d713b00d399288349fbd
svaldes99 <s.valdes1@uc.cl>
2022-03-28 16:08:11 -0300

    retweeted

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from retweeted import retweeted
+
 def main():
     """ Main program """
-    # Code goes over here.
+    option = input('Please select an option: \n'
+                   '(1) Top 10 tweets más retweeted \n'  
+                   '(2) Top 10 usarios con más tweets \n'  
+                   '(3) Top 10 de días con más tweets \n'
+                   '(4) Top 10 de hashtags más usados \n')
+    if option == '1':
+        retweeted()
     return 0
 
 if __name__ == "__main__":
